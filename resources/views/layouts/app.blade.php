ec20dcdc25a30fddef2d20bb3783245b63d0da39
Juan Carlos <juan.carlos@rakwireless.com>
2020-02-24 16:09:12 +0800

    add: banner icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                <img class="avatar rounded-circle avatar-sm" src="{{asset('/img/1.png')}}" alt="">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
