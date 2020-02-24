132afdb970c5e157d0d05cc3eb8f1df7795e2137
Juan Carlos <juan.carlos@rakwireless.com>
2020-02-24 18:21:29 +0800

    mod: design

--- resources/views/layouts/sections/table.blade.php
+++ resources/views/layouts/sections/table.blade.php
@@ -1,19 +1,160 @@
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-4">
+            <div class="card" style="background-color:black;">
+                <div class="card-header text-center"> <img class="avatar rounded-circle avatar-lg" src="{{asset('/img/67223996_499982087473958_7499499278092468224_n.png')}}" alt=""></div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 p-2">
+                                <button type="button" class="btn btn-secondary btn-lg btn-icon text-left" style="width:100%;">
+                                    <span class="btn-inner--icon">
+                                        <img class="avatar rounded-circle avatar-sm" src="{{asset('/img/1.png')}}" alt="">
+                                    </span>
+                                    <span class="btn-inner--text">MENU</span>
+                                </button>
+                            </div>
                         </div>
-                    @endif
-
-                    <a href="/students">Table</a>
+                        <div class="row">
+                            <div class="col-12 p-2">
+                                <button type="button" class="btn btn-secondary btn-lg btn-icon text-left" style="width:100%;">
+                                    <span class="btn-inner--icon">
+                                        <img class="avatar rounded-circle avatar-sm" src="{{asset('/img/bill-icon.png')}}" alt="">
+                                    </span>
+                                    <span class="btn-inner--text">BILL </span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 p-2">
+                                <button type="button" class="btn btn-secondary btn-lg btn-icon text-left" style="width:100%;">
+                                    <span class="btn-inner--icon">
+                                        <img class="avatar rounded-circle avatar-sm" src="{{asset('/img/about-icon.png')}}" alt="">
+                                    </span>
+                                    <span class="btn-inner--text">ABOUT</span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 p-2 text-center">
+                              <a href=""><img class="img-fluid mt-5" src="{{asset('/img/call-server.png')}}" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-12">
+                            <div class="text-center">
+                              <h3 class="text-white mb-4"><strong>TABLE 1</strong></h3>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
+        <div class="col-md-8">
+            <div class="p-1"style="background-color: white;">
+                  <div class="container p-0">
+                    <div class="text-center p-3" style="background-color: #B22C2D;">
+                      <h3 class="text-dark"><strong>MENU</strong></h3>
+                    </div>
+                    <div class="row">
+                      <div class="col-lg-6">
+                        <div class="card hover-shadow-lg hover-translate-y-n10"style="height:300px;">
+                          <div class="card-image p-0">
+                            <img alt="Image placeholder" src="{{asset('/img/meat.png')}}" class="card-img mt-3 p-6">
+                          </div>
+                          
+                          
+                          <div class="card-img-overlay d-flex flex-column">
+                            <div class="col pt-2">
+                              <h3 class="text-capitalize mb-1">
+                                <a href="#" class="text-dark">MEAT</a>
+                              </h3>
+                              {{-- <h6 class="text-dark">
+                                <a href="#" class="text-dark border-bottom border-white">Identity &amp; Web Design</a>
+                              </h6> --}}
+                            </div>
+                            {{-- <div class="w-100 mt-auto text-right">
+                              <span class="text-dark font-weight-bold">Jun 20, 2019</span>
+                            </div> --}}
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-lg-6">
+                        <div class="card hover-shadow-lg hover-translate-y-n10" style="height:300px;">
+                          <div class="card-image p-0">
+                            <img alt="Image placeholder" src="{{asset('/img/side-dish.png')}}" class="card-img p-7">
+                          </div>
+                          
+                          
+                          <div class="card-img-overlay d-flex flex-column">
+                            <div class="col pt-2">
+                              <h3 class="text-capitalize mb-1">
+                                <a href="#" class="text-dark">SIDE DISH</a>
+                              </h3>
+                              {{-- <h6 class="text-dark">
+                                <a href="#" class="text-dark border-bottom border-white">Branding</a>
+                              </h6> --}}
+                            </div>
+                            {{-- <div class="w-100 mt-auto text-right">
+                              <span class="text-dark font-weight-bold">May 31, 2019</span>
+                            </div> --}}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-lg-6">
+                        <div class="card hover-shadow-lg hover-translate-y-n10" style="height:300px;">
+                          <div class="card-image p-0">
+                            <img alt="Image placeholder" src="{{asset('/img/dessert.png')}}" class="card-img p-7">
+                          </div>
+                          
+                          
+                          <div class="card-img-overlay d-flex flex-column">
+                            <div class="col pt-2">
+                              <h3 class="text-capitalize mb-1">
+                                <a href="#" class="text-dark">DESSERT</a>
+                              </h3>
+                              {{-- <h6 class="text-dark">
+                                <a href="#" class="text-dark border-bottom border-white">Marketing</a>
+                              </h6> --}}
+                            </div>
+                            {{-- <div class="w-100 mt-auto text-right">
+                              <span class="text-dark font-weight-bold">May 18, 2019</span>
+                            </div> --}}
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-lg-6">
+                        <div class="card hover-shadow-lg hover-translate-y-n10" style="height:300px;">
+                          <div class="card-image p-0">
+                          <img alt="Image placeholder" src="{{asset('/img/beverages.png')}}" class="card-img p-7">
+                          </div>
+                          
+                          
+                          <div class="card-img-overlay d-flex flex-column">
+                            <div class="col pt-2">
+                              <h3 class="text-capitalize mb-1">
+                                <a href="#" class="text-dark">BEVERAGES</a>
+                              </h3>
+                              {{-- <h6 class="text-dark">
+                                <a href="#" class="text-dark border-bottom border-white">Management</a>
+                              </h6> --}}
+                            </div>
+                            {{-- <div class="w-100 mt-auto text-right">
+                              <span class="text-dark font-weight-bold">Apr 23, 2019</span>
+                            </div> --}}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+        </div>
     </div>
 </div>
+
